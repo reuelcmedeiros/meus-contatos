47b2b0e5a4fc26dad3d973968fead9770acd9f1c
KauaBNg <88908164+KauaBNg@users.noreply.github.com>
2026-04-10 19:35:10 -0300

    Delete sc.js

--- sc.js
+++
@@ -1,8 +0,0 @@
-let foto = document.getElementById("foto");
-let nome = document.getElementById("nome");
-
-foto.addEventListener("click", function () {
-  foto.style.transform = "scale(3)";
-  nome.style.opacity = "0";
-});
-
