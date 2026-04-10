0841619a3afbe5160f7a45552bdf8030c9fca7d9
KauaBNg <88908164+KauaBNg@users.noreply.github.com>
2026-04-09 21:19:10 -0300

    js 3

--- sc.js
+++ sc.js
@@ -1,4 +1,8 @@
 let foto = document.getElementById('foto');
+let nome = document.getElementById('nome');
+
 foto.addEventListener('click', function(){
 foto.style.transform="scale(2)";
+nome.style.opacity = "0.1";
 })
+
